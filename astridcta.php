b84fec2ec6aadc9f7199687a14aa0fceaa83ff62
Jon Paris <jon@todoroo.com>
2012-10-14 21:14:28 -0700

    add options page to make the header to the plugin page configurable

--- astridcta.php
+++ astridcta.php
@@ -10,7 +10,6 @@ License: GPLv2
 /* Directories and URLs */
 define( 'ACTA_URL', plugin_dir_url(__FILE__) );
 define( 'ACTA_DIR', plugin_dir_path(__FILE__) );
-define('DEFAULT_REMINDER', 3);
 
 class AstridCTA {
 	static $instance = false;
@@ -105,39 +104,40 @@ class AstridCTA {
 		return $new;
 	}
 	
-function acta_content_footer( $content ) {
-	global $post;
 
-	if ( is_singular() && is_main_query() ) {
-		$actions = get_post_meta( $post->ID, 'acta_actions', true );
-		if ( $actions && is_array( $actions ) && count( $actions ) > 0 ) {
-			$content .= '<div id="acta_actions_fe">';
-			$content .= '<h2>Don\'t forget!</h2>';
-			$content .= '<p>Get reminders by email or through <a href="http://astrid.com">Astrid</a> for iPhone, iPad, or Android.</p>';
-			$content .= '<ul>';
-			$siteurl = get_site_url();
-			$step = 0; 
-			foreach( $actions as $action ) { 
-				$step += 1;
-				$content .= '<li id="acta_action_fe_' . $step . '" class="acta_action_fe">';
-				$content .= $action['text'];
-				$content .= '&nbsp;&nbsp;';
-				$content .= '<iframe allowtransparency="true" frameborder="0" scrolling="no"height="21px" width="116px" title="Astrid Remind Me"';
-				$content .= 'src="http://astrid.com/widgets/remind_me?title=' . urlencode($action['text']);
-				$content .= '&due_in_days=' . $action['reminder_days'];
-				$content .= '&notes='.urlencode($action['notes']);
-				$content .= '&source_name=';
-				$content .= '&source_url='.urlencode(post_permalink());
-				$content .= '&suggester_id=45&button_size=mini&button_style=astrid&button_title=Remind%20me"></iframe>';
-				$content .= '</li>';
+	function acta_content_footer( $content ) {
+		global $post;
+		$author_username = get_option("astrid_author_username");
+		if ( is_singular() && is_main_query() ) {
+			$actions = get_post_meta( $post->ID, 'acta_actions', true );
+			if ( $actions && is_array( $actions ) && count( $actions ) > 0 ) {
+				$content .= '<div id="acta_actions_fe">';
+				$content .= '<h2>' . get_astrid_cta_option('header') . '</h2>';
+				$content .= '<p>' . get_astrid_cta_option('description') .'</p>';
+				$content .= '<ul>';
+				$siteurl = get_site_url();
+				$step = 0; 
+				foreach( $actions as $action ) { 
+					$step += 1;
+					$content .= '<li id="acta_action_fe_' . $step . '" class="acta_action_fe">';
+					$content .= $action['text'];
+					$content .= '&nbsp;&nbsp;';
+					$content .= '<iframe allowtransparency="true" frameborder="0" scrolling="no"height="21px" width="116px" title="Astrid Remind Me"';
+					$content .= 'src="http://astrid.com/widgets/remind_me?title=' . urlencode($action['text']);
+					$content .= '&due_in_days=' . $action['reminder_days'];
+					$content .= '&notes='.urlencode($action['notes']);
+					$content .= '&source_name=';
+					$content .= '&source_url='.urlencode(post_permalink());
+					$content .= '&suggester_id=45&button_size=mini&button_style=astrid&button_title=Remind%20me"></iframe>';
+					$content .= '</li>';
+				}
+				$content .= '</ul>';
+				$content .= '</div>';
 			}
-			$content .= '</ul>';
-			$content .= '</div>';
 		}
-	}
 
-	return $content;
-}
+		return $content;
+	}
 	
 	public static function get_instance() {
         if ( !self::$instance ) {
@@ -149,4 +149,60 @@ function acta_content_footer( $content ) {
 
 AstridCTA::get_instance();
 
+/*** Admin Panel ***/
+add_action('admin_init', 'astrid_cta_init' );
+add_action('admin_menu', 'astrid_cta_add_page');
+
+function get_astrid_cta_option($option) {
+	$option_default = array(
+    	"header" => "Don\'t forget!",
+    	"description" => 'Get reminders by email or through <a href="http://astrid.com">Astrid</a> 
+    						for iPhone, iPad, or Android.'
+	);
+	$options = get_option('astrid_cta');
+	return $options[$option] ? $options[$option] : $option_default[$option];
+}
+
+// Init plugin options to white list our options
+function astrid_cta_init(){
+	register_setting( 'astrid_cta_options', 'astrid_cta', 'astrid_cta_validate' );
+}
+
+// Add menu page
+function astrid_cta_add_page() {
+	add_options_page('Astrid Calls To Action', 'Astrid Calls-to-Action', 'manage_options', 'astrid_cta', 'astrid_cta_do_page');
+}
+
+// Draw the menu page itself
+function astrid_cta_do_page() {
+	?>
+	<div class="wrap">
+		<h2>Astrid Calls To Action</h2>
+		<form method="post" action="options.php">
+			<?php settings_fields('astrid_cta_options'); ?>
+		</p>
+			<table class="form-table">
+				<tr valign="top"><th scope="row">CTA Header</th>
+					<td><input name="astrid_cta[header]" type="text" value="<?php echo get_astrid_cta_option('header'); ?>" /></td>
+				</tr>
+				<tr valign="top"><th scope="row">Description</th>
+					<td><textarea name="astrid_cta[description]" rows="3"><?php echo get_astrid_cta_option('description'); ?></textarea></td>
+				</tr>
+			</table>
+			<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			</p>
+		</form>
+	</div>
+	<?php	
+}
+
+// Sanitize and validate input. Accepts an array, return a sanitized array.
+function astrid_cta_validate($input) {
+	// Say our second option must be safe text with no HTML tags
+	$input['header'] =  wp_filter_nohtml_kses($input['header']);
+	$input['description'] =  wp_filter_nohtml_kses($input['description']);
+	
+	return $input;
+}
 ?>
